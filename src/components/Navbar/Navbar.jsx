a8f237dd91e6007c083dc8235aa83dcfd3f8f6d0
andrejarboe <andrejarboe@gmail.com>
2018-10-05 18:53:55 -0400

    fixed nav res

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
 
-
-import './Navbar.css'
-
-
+import "./Navbar.css";
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav>
         <div className="navbar wrapper">
-        
-        <div className="logo">
-          <h1>Jingles</h1>
-        </div>
-        <div className="links">
-          <Link to="/products">Products</Link>
-          <Link to="/products">About</Link>
-          <Link to="/products">FAQ</Link>
-          <Link to="/products">Request a quote</Link>
-        </div>
+          <div className="logo">
+            <h1>Jingles</h1>
+          </div>
+          <div className="links">
+            <Link to="/products">Products</Link>
+            <Link to="/products">About</Link>
+            <Link to="/products">FAQ</Link>
+            <Link to="/products">Request a quote</Link>
+          </div>
         </div>
       </nav>
     );
