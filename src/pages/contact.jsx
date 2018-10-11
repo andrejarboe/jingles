c432f735f6b3c03ecd2711e3065e3fd8b314730c
andrejarboe <andrejarboe@gmail.com>
2018-10-11 14:42:06 -0400

    add contact page bg

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,31 +1,40 @@
 import React, { Component } from "react";
 import Layout from "../components/layout";
-import Img from "gatsby-image"
+import Img from "gatsby-image";
+import styled from "styled-components";
+
+import "../css/contact.css";
 
 export default function ContactPage({ data }) {
-
-
-    return (
-      <Layout>
-        <section id="contact-hero">
-          <Img fluid={data.amplifier.childImageSharp.fluid} />
+  return (
+    <Layout>
+      <section id="contact-hero">
+        <div className="contact-container">
           <h1>Contact</h1>
-        </section>
-      </Layout>
-    );
-  
+        </div>
+        <Img
+          style={{
+            position: "absolute",
+            left: 0,
+            top: 0,
+            width: "100%",
+            height: "100%"
+          }}
+          fluid={data.background.childImageSharp.fluid}
+        />
+      </section>
+    </Layout>
+  );
 }
-
-
 
 export const contactQuery = graphql`
   query {
-    amplifier: file(relativePath: { eq: "amplifier.jpg" }) {
+    background: file(relativePath: { eq: "contactBg.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 1000) {
+        fluid(maxWidth: 2000) {
           ...GatsbyImageSharpFluid
         }
       }
     }
   }
-`
+`;
